fb1f2f2c98d322d88cee1e4d3e317c1da92a58e5
felix.blanchette@gmail.com <felix.blanchette@gmail.com>
2024-04-12 12:51:05 -0400

    La vue partielle existe maintenant pour vrai de vrai lala

--- Controllers/NonConnecteController.cs
+++ Controllers/NonConnecteController.cs
@@ -17,7 +17,8 @@ namespace TP2_final.Controllers
         public NonConnecteController(ILogger<NonConnecteController> logger)
         {
             _logger = logger;
-            if (isSerializationToDo) {
+            if (isSerializationToDo)
+            {
                 isSerializationToDo = false;
                 catalogueUtilisateur = new CatalogueUtilisateur();
 
@@ -31,15 +32,18 @@ namespace TP2_final.Controllers
         }
 
         [HttpPost]
-        public IActionResult Connecte() {
-            String pseudo =  Request.Form["connPseudo"];
+        public IActionResult Connecte()
+        {
+            String pseudo = Request.Form["connPseudo"];
             String mdp = Request.Form["connMdp"];
 
             Utilisateur user = catalogueUtilisateur.GetUtilisateurByPseudo(pseudo);
-            if (user is null || user.MotDePasse != mdp) {
+            if (user is null || user.MotDePasse != mdp)
+            {
                 Console.WriteLine($"ERREUR DE CONN, user existe pas:{user is null} | mauvais pw: {(user?.MotDePasse != mdp)}");
                 return View("MARCHE PAS, JAR SA EXPLOSE C SUR");
-            } else
+            }
+            else
             {
                 Console.WriteLine($"CONN pseudo:{pseudo}, mdp:{mdp}, id:{user.getId()}");
                 TempData.Clear();
@@ -52,14 +56,16 @@ namespace TP2_final.Controllers
         }
 
         [HttpPost]
-        public IActionResult Inscrire() {
+        public IActionResult Inscrire()
+        {
             // TODO CHECK IF ALL INPUTS ARE GOOD
             // TODO CHECK IF USER ALREADY EXISTS
             if (false/*TODO*/)
             {
                 Console.WriteLine($"TODO, ERREUR INSCRIPTION, inputs invalides:{"'TODO'"}, user existe deja:{"'TODO'"}");
             }
-            else {
+            else
+            {
                 Utilisateur newUser = new Utilisateur(Request.Form["insPseudo"], Request.Form["insMdp"], Request.Form["insNomFamille"], Request.Form["insPrenom"], Utilisateur.ROLE_DEFAULT);
                 //chek si le user existe pas
                 String pseudo = Request.Form["insPseudo"];
@@ -84,6 +90,11 @@ namespace TP2_final.Controllers
                     return RedirectToAction("Index", newUser.Role.ToString().ToLower(), catalogueUtilisateur);
                 }
             }
+        }
+
+        public IActionResult VuePartielleErreur()
+        {
+            return View("VuePartielleErreur");
         }
 
 
