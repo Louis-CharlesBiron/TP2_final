b6e70f58ba8f9667eb779f5933d91b91cfa7e738
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-04-16 12:52:43 -0400

    filtration données done reste à implementer

--- Controllers/NonConnecteController.cs
+++ Controllers/NonConnecteController.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using TP2_final.Models;
 using System.Text.RegularExpressions;
+using System.Text.Encodings.Web;
+using System.Web;
 
 namespace TP2_final.Controllers
 {
@@ -25,9 +27,10 @@ namespace TP2_final.Controllers
             return View();
         }
 
-        private string ValidationPseudo(String pseudo) {
+        private string ValidationPseudo(String pseudo)
+        {
             //TODO, vérification && // filtrage
-
+            pseudo = Filtrage(pseudo);
             return !(pseudo.Length >= 5 &&
                pseudo.Length <= 50 &&
                new Regex("[a-z]", RegexOptions.IgnoreCase).IsMatch(pseudo) &&
@@ -38,18 +41,23 @@ namespace TP2_final.Controllers
                : pseudo;
         }
 
-        private string ValidationPassword(String pw) {
+        private string ValidationPassword(String pw)
+        {
             //TODO
+            pw = Filtrage(pw);
+
             return pw;
         }
 
-        private string ValidationPrenom(String prenom) {
+        private string ValidationPrenom(String prenom)
+        {
             //TODO
             return prenom;
         }
 
 
-        private string ValidationNom(String nom) {
+        private string ValidationNom(String nom)
+        {
             //TODO
             return nom;
         }
@@ -62,13 +70,14 @@ namespace TP2_final.Controllers
             Utilisateur? user = catalogueUtilisateur.GetUtilisateurByPseudo(pseudo);
 
             //Validation champs
-            if (string.IsNullOrEmpty(pseudo)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le pseudo est invalide"});
-            if (string.IsNullOrEmpty(mdp)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le mot de passe est invalide"});
+            if (string.IsNullOrEmpty(pseudo)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le pseudo est invalide" });
+            if (string.IsNullOrEmpty(mdp)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le mot de passe est invalide" });
 
             //Validation connection
-            if (user is null) return RedirectToAction("Erreur", "NonConnecte", new {msg="L'utilisateur n'existe pas"});
-            else if (user.MotDePasse != mdp) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le mot de passe est invalide"});  
-            else {
+            if (user is null) return RedirectToAction("Erreur", "NonConnecte", new { msg = "L'utilisateur n'existe pas" });
+            else if (user.MotDePasse != mdp) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le mot de passe est invalide" });
+            else
+            {
                 Console.WriteLine($"CONN pseudo:{pseudo}, mdp:{mdp}, id:{user.getId()}");
                 TempData.Clear();
                 TempData["username"] = user.Pseudo;
@@ -87,14 +96,15 @@ namespace TP2_final.Controllers
             string mdp = ValidationPassword(Request.Form["insMdp"]);
 
             // si champs sont valides
-            if (string.IsNullOrEmpty(pseudo)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le pseudo est invalide"});
-            if (string.IsNullOrEmpty(mdp)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le mot de passe est invalide"});
-            if (string.IsNullOrEmpty(prenom)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le prénom est invalide"});
-            if (string.IsNullOrEmpty(nom)) return RedirectToAction("Erreur", "NonConnecte", new {msg="Le nom de famille est invalide"});
+            if (string.IsNullOrEmpty(pseudo)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le pseudo est invalide" });
+            if (string.IsNullOrEmpty(mdp)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le mot de passe est invalide" });
+            if (string.IsNullOrEmpty(prenom)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le prénom est invalide" });
+            if (string.IsNullOrEmpty(nom)) return RedirectToAction("Erreur", "NonConnecte", new { msg = "Le nom de famille est invalide" });
 
             // si user existe déja
-            if (catalogueUtilisateur.GetUtilisateurByPseudo(pseudo) is not null)  return RedirectToAction("Erreur", "NonConnecte", "Un utilisateur ayant le même pseudo existe déja");
-            else {
+            if (catalogueUtilisateur.GetUtilisateurByPseudo(pseudo) is not null) return RedirectToAction("Erreur", "NonConnecte", "Un utilisateur ayant le même pseudo existe déja");
+            else
+            {
                 //Création new user avec données d'inscription
                 Utilisateur newUser = new Utilisateur(pseudo, mdp, nom, prenom, Utilisateur.ROLE_DEFAULT);
                 catalogueUtilisateur.Ajouter(newUser);
@@ -106,16 +116,27 @@ namespace TP2_final.Controllers
             }
         }
 
-        public IActionResult Erreur(string msg) {
+        public IActionResult Erreur(string msg)
+        {
             TempData["erreurs"] = msg;
 
-            return RedirectToAction("Index", "NonConnecte");   
+            return RedirectToAction("Index", "NonConnecte");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string Filtrage(string input)
+        {
+            /*
+             * conversion entité html
+             * enlever les echappements
+             * enlever les espaces blancs avant et apres
+             */
+            return HttpUtility.HtmlEncode(Regex.Unescape(input.Trim()));
         }
     }
 }
